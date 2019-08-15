888ad0558f77d1422dd876040a72a3f971cb0021
Karina Macancela <karymacancela@gmail.com>
2019-08-15 14:58:14 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -10,4 +10,6 @@ second_number = 1
 sum = first_number + second_number
 
 # Fourth, fifth, and sixth tests
-difference = 
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
